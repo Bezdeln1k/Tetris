c07d2b0b134360f240eac5346d8adfa354e057ab
Alexander Borloev <borloev.1994@gmail.com>
2023-08-20 11:33:24 +0900

    Добавили класс Square (для фигуры квадрат)

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -8,16 +8,19 @@ internal class Program
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Point p1 = new Point(2, 3, '*');
-            p1.Draw();
+            Square s = new Square(2, 5, '*');
+            s.Draw();
 
-            Point p2 = new Point()
-            {
-                x = 4,
-                y = 5,
-                c = '#'
-            };
-            p2.Draw();
+            //Point p1 = new Point(2, 3, '*');
+            //p1.Draw();
+
+            //Point p2 = new Point()
+            //{
+            //    x = 4,
+            //    y = 5,
+            //    c = '#'
+            //};
+            //p2.Draw();
 
 
 
