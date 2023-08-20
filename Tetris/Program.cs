17dbf576b5a3b7865b57223092951989804f2cd2
Alexander Borloev <borloev.1994@gmail.com>
2023-08-20 12:22:36 +0900

    Описали родительский класс Figure и перенесли общие параметры из Square и Stick

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -8,11 +8,20 @@ internal class Program
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Square s = new Square(2, 5, '*');
-            s.Draw();
+            Figure[] f = new Figure[2];
+            f[0] = new Square(2, 5, '*');
+            f[1] = new Stick(7, 7, '*');
 
-            Stick st = new Stick(7, 7, '*');
-            st.Draw();
+            foreach(Figure fig in f)
+            {
+                fig.Draw();
+            }
+
+            //Square s = new Square(2, 5, '*');
+            //s.Draw();
+
+            //Stick st = new Stick(7, 7, '*');
+            //st.Draw();
 
             //Point p1 = new Point(2, 3, '*');
             //p1.Draw();
