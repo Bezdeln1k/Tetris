bec47a816b60c653bced91996be98675afd2e625
Alexander Borloev <borloev.1994@gmail.com>
2023-08-22 21:07:13 +0900

    Научились использовать класс Random в отдельном проекте Randomizer

---
+++ Randomizer/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Randomizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            //int value = rand.Next(0, 5);
+            //Console.WriteLine(value);
+
+            for(int i = 0; i < 20;  i++)
+            {
+                int value = rand.Next(0, 5);
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
