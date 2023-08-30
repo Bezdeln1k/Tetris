8a6994c2ec1dfbf063c3da5a67ffe46fb071f8c8
Alexander Borloev <borloev.1994@gmail.com>
2023-08-30 15:50:40 +0900

    Познакомились с ключевым словом var

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -10,29 +10,18 @@ internal class Program
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure? s; // знак ? говорит компилятору, что допускается значение null. 
+            Figure currentFigure = generator.GetNewFigure(); 
 
             while (true)
             {
-                FigureFall(out s, generator); // s - ссылка на область опер.памяти. Чтобы ссылка поменялась (и после выполнения ф-и FigureFall
-                                              // переменная s приняла значение переменной fig) нужно ставить ключевое слово ref; либо out - если
-                                              // разработчик уверен, что переменная с ключевым словом out будет задана
-                s.Draw();
+                if (Console.KeyAvailable)   //"Улавливает" нажатие клавиши в консоли
+                {
+                    //ConsoleKeyInfo key = Console.ReadKey();     //переменная key хранит значение, которое нажал пользователь
+                    var key = Console.ReadKey();                  // ключевое слово var "заменяет" любой тип и компилятор сам определяет тип данных
+
+                }
             }
 
-        }
-        static void FigureFall(out Figure fig, FigureGenerator generator)
-        {
-            fig = generator.GetNewFigure();
-            fig.Draw();
-
-            for (int i = 0; i < 15; i++)
-            {
-                fig.Hide();
-                fig.Move(Direction.DOWN);
-                fig.Draw();
-                Thread.Sleep(200);
-            }
         }
 
     }
